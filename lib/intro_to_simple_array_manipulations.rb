66694de7171c176013747205dad8b6852ef21745
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-03 22:21:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(instructors, string)
   instructors.delete(string)
 end
 
-def using_delete_at(famous_robots, 2)
+def using_delete_at(famous_robots)
   famous_robots.delete(2)
 end
