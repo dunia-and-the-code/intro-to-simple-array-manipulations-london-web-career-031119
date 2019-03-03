325f3bf5a7d14a5f5c1c644e4305a53e4eb10a9d
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-03 21:32:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,4 +10,6 @@ def using_pop(great_hits_of_the_nineties)
   great_hits_of_the_nineties.pop
 end
 
-def pop_with_args()
+def pop_with_args(chars_in_game_of_thrones)
+  chars_in_game_of_thrones.pop(2)
+end
